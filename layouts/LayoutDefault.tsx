43e9ede6424c469ee87d32c82eca1b904985108b
Nikol Angelov <nicole.angelov396@gmail.com>
2023-12-15 16:18:55 +0200

    Header implemented

--- layouts/LayoutDefault.tsx
+++ layouts/LayoutDefault.tsx
@@ -1,40 +1,34 @@
 import 'uno.css'
 import "./style.css";
-import logoUrl from "../assets/logo.svg";
+
 import { Link } from "../components/Link";
 import type { JSX } from "solid-js";
 
+function MenuItem(props: {href:string, children: JSX.Element }) {
+return <a href={props.href}class="c-white">{props.children}</a>
+}
+
 export default function LayoutDefault(props: { children?: JSX.Element }) {
   return (
-    <div
-      style={{
-        display: "flex",
-        "max-width": "900px",
-        margin: "auto",
-      }}
+    <div class="flex flex-col"
     >
-      <Sidebar>
+      <Topbar>
         <Logo />
-        <Link href="/">Welcome</Link>
-        <Link href="/star-wars">Data Fetching</Link>
-      </Sidebar>
+        <div class="flex flex-justify-end flex-items-center">
+          <MenuItem href="/">Welcome</MenuItem>
+          <MenuItem href="/star-wars">Data Fetching</MenuItem>
+        </div>
+      </Topbar>
       <Content>{props.children}</Content>
     </div>
   );
 }
 
-function Sidebar(props: { children: JSX.Element }) {
+function Topbar(props: { children: JSX.Element }) {
   return (
     <div
       id="sidebar"
-      style={{
-        padding: "20px",
-        "flex-shrink": 0,
-        display: "flex",
-        "flex-direction": "column",
-        "line-height": "1.8em",
-        "border-right": "2px solid #eee",
-      }}
+      class="flex p-4 flex-shrink-0 line-height-7 border-b-2 bg-blue-9 flex-justify-between"
     >
       {props.children}
     </div>
@@ -67,7 +61,7 @@ function Logo() {
       }}
     >
       <a href="/">
-        <img src={logoUrl} height={64} width={64} />
+        <img src="/assets/logo.png" width={150} height={43} />
       </a>
     </div>
   );
--- pages/index/+Page.tsx
+++ pages/index/+Page.tsx
@@ -3,14 +3,25 @@ import { Counter } from "./Counter";
 export default function Page() {
   return (
     <>
-      <h1 class="c-green">My vike-solid app</h1>
+      <h1 class="c-red flex-wrap">My vike-solid app</h1>
       This page is:
-      <ul>
-        <li>Rendered to HTML.</li>
-        <li>
+      <ul class="flex flex-row-reverse">
+        <li class="w-26">Rendered to HTML.</li>
+        <li class="w-26">  
           Interactive. <Counter />
         </li>
       </ul>
+      
+     <div style="background-color:#ff0066;padding:20px;"> 
+      <form>
+      <label for="fname">First name:&#128509;</label>
+<input type="text" id="fname" name="fname"></input><br></br>
+<label for="lname">Last name:&#128509;</label>
+<input type="text" id="lname" name="lname"></input><br></br>
+<input type="submit" value="Submit"></input>
+</form>
+</div>
+
     </>
   );
 }
