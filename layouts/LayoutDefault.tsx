fb2e871dc9f8e80820bdc103ff1aab69fc317a64
Nikol Angelov <nicole.angelov396@gmail.com>
2024-03-05 09:38:28 +0200

    Смалих още височината на хедъра, махнах разстоянието между първата снимка (синята) и хедъра. Увеличих пространството между различните елементи на началната страница да има повече white space и започнах правя футъра.

--- layouts/LayoutDefault.tsx
+++ layouts/LayoutDefault.tsx
@@ -4,7 +4,7 @@ import type { JSX } from "solid-js";
 import { createSignal,Show } from "solid-js";
 
 function MenuItem(props: {href:string, children: JSX.Element }) {
-return <a href={props.href} class="flex sm-hidden lg-flex top-bar-meniitem h-13 c-paper text-center font-size-4 xl-font-size-4.5 uppercase">{props.children}</a>
+return <a href={props.href} class="flex sm-hidden lg-flex top-bar-meniitem h-14 c-paper text-center font-size-4 xl-font-size-4.8 uppercase">{props.children}</a>
 }
 
 export default function LayoutDefault(props: { children?: JSX.Element }) {
@@ -25,6 +25,23 @@ export default function LayoutDefault(props: { children?: JSX.Element }) {
         </div>
       </Topbar>
       <Content>{props.children}</Content>
+      <MainFooter>
+        <MainFooterSocialIcons></MainFooterSocialIcons>
+        <MainFooterMenuItem href="/">Какво е Фалун Дафа</MainFooterMenuItem>
+        <MainFooterMenuItem href="/">Защо е преследван</MainFooterMenuItem>
+        <MainFooterMenuItem href="/">Отнемане на органи</MainFooterMenuItem>
+        <MainFooterMenuItem href="/">По света</MainFooterMenuItem>
+        <MainFooterMenuItem href="/">В България</MainFooterMenuItem>
+        <MainFooterMenuItem href="/">Видеа</MainFooterMenuItem>
+      </MainFooter>
+      <BottomFooter>
+        <div class="flex flex-row">
+          <MainFooterMenuItem href="/">За нас</MainFooterMenuItem>
+          <MainFooterMenuItem href="/">Политика за поверителност</MainFooterMenuItem>
+          <MainFooterMenuItem href="/">Условия за ползване</MainFooterMenuItem>
+        </div>
+        <p class="c-paper">Copyright &copy; 2000-2024 - Фалун Дафа информационен център - Всички права запазени.</p>
+      </BottomFooter>
     </div>
   );
 }
@@ -33,7 +50,7 @@ function Topbar(props: { children: JSX.Element }) {
   return (
     <div
       id="sidebar"
-      class="flex p-8 flex-shrink-0 line-height-5 border-b-2 bg-brand flex-justify-between flex-items-center">
+      class="flex p-5 position-sticky top-0 flex-shrink-0 line-height-5 border-b-2 bg-brand flex-justify-between flex-items-center">
       {props.children}
     </div>
   );
@@ -85,3 +102,45 @@ function HamburgerMenu(props: { children: JSX.Element }) {
     </button>
   )
 }
+
+function MainFooter(props: { children: JSX.Element }) {
+  return (
+    <div
+      class="flex flex-col bg-brand-dark p-10 flex-shrink-0 flex-justify-between flex-items-center">
+      {props.children}
+    </div>
+  );
+}
+
+function MainFooterMenuItem(props: {href:string, children: JSX.Element }) {
+  return <a href={props.href} class="flex c-paper text-center font-normal font-size-4.5 py-1.5 hover-color-paper-link-hover:hover" style="font-family: Open Sans, sans-serif;">{props.children}</a>
+  }
+
+    function MainFooterSocialIcons() {
+      return (
+        <div class="flex grid-items-center mb-7"
+        >
+          <a class="mr-7 ml-7" href="/">
+            <img src="\assets\SVG Icons\youtube.svg" width={50} height={50} />
+          </a>
+          <a class="mr-7 ml-7" href="/">
+            <img src="\assets\SVG Icons\facebook.svg" width={40} height={40} />
+          </a>
+          <a class="mr-7 ml-7" href="/">
+            <img src="\assets\SVG Icons\instagram.svg" width={45} height={45} />
+          </a>
+          <a class="mr-7 ml-7" href="/">
+            <img src="\assets\SVG Icons\square-x-twitter.svg" width={45} height={45} />
+          </a>
+        </div>
+      );
+    }
+
+    function BottomFooter(props: { children: JSX.Element }) {
+      return (
+        <div
+          class="flex flex-col bg-brand p-10 flex-shrink-0 flex-justify-between flex-items-center">
+          {props.children}
+        </div>
+      );
+    }
